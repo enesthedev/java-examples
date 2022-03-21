28cd2d0e5509b312476c687c0dbbecf34451d3da
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-21 22:49:30 +0100

    Add: Error messages to methods
    Refactor: Changed fire method return type void to boolean
    
    Add: Usefully comments for homework

--- src/com/bpr1210945/applications/gunandplay/classes/Clip.java
+++ src/com/bpr1210945/applications/gunandplay/classes/Clip.java
@@ -1,8 +1,20 @@
 package com.bpr1210945.applications.gunandplay.classes;
 
-public class Clip {
+public class Clip {	
 	int bulletCapacity;
 	int bulletCount;
+	
+	/**
+	 * Şarjör sınıfı
+	 * 
+	 * Bu sınıfta şajörün mermi kapasitesini ve mermi sayısını belirten iki değişken atadım, normalde bu değişkenlerin getter ve setterları ile
+	 * silah sınıfında ateşleme metodları yazıcaktım ancak ödevde şarjör içinde olmasını istediğiniz için getter ve setterlara gerek kalmadı.
+	 * 
+	 * Hata mesajlarında silah sınıfı ile direkt bir ilişki içerisinde olduğu için Şarjör yerine silah demeyi uygun gördüm.
+	 * 
+	 * Silahın ateşlendiği metoda true/false tipinde geri dönüş verdim çünkü kendimce bir hedef sistemi yazmak istedim
+	 * bu durumda hedefin vurulup vurulmadığını algılamam gerekiyordu.
+	 */
 	
 	public Clip(int bulletCapacity, int bulletCount) {
 		this.bulletCapacity = bulletCapacity;
@@ -10,12 +22,21 @@ public class Clip {
 	}
 	
 	public void reload() {
-		if (this.bulletCapacity - this.bulletCount > 0)
+		if (this.bulletCapacity - this.bulletCount > 0) {
 			this.bulletCount = this.bulletCapacity;
+			return;
+		}
+		
+		System.out.println("Silah zaten dolu.");
 	}
 
-	public void fire() {
-		if (this.bulletCount > 0)
+	public Boolean fire() {
+		if (this.bulletCount > 0) {
 			this.bulletCount -= 1;
+			return true;
+		}
+		
+		System.out.println("Silahta mermi yok!");
+		return false;
 	}
 }
